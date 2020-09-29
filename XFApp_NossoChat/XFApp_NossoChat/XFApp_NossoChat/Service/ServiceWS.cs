72c96e7c8b5e0007c2a5930f1cab1e0862f3471b
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-09-29 10:18:19 -0300

    29.09.2020_10:18hs

--- XFApp_NossoChat/XFApp_NossoChat/XFApp_NossoChat/Service/ServiceWS.cs
+++ XFApp_NossoChat/XFApp_NossoChat/XFApp_NossoChat/Service/ServiceWS.cs
@@ -4,15 +4,17 @@ using System.Text;
 using System.Net;
 using System.Net.Http;
 using XFApp_NossoChat.Model;
+using Newtonsoft.Json;
 
 namespace XFApp_NossoChat.Service
 {
     public class ServiceWS
     {
         private static string EnderecoBase = "http://ws.spacedu.com.br/xf2018/rest/api";
+
         public static Usuario GetUsuario(Usuario usuario)
         {
-            var Url = EnderecoBase + "/usuario";
+            var URL = EnderecoBase + "/usuario";
 
             /*
              * Usando QueryString:
@@ -25,7 +27,7 @@ namespace XFApp_NossoChat.Service
             });
 
             HttpClient requisicao = new HttpClient();
-            HttpResponseMessage resposta = requisicao.PostAsync(Url, param).GetAwaiter().GetResult();
+            HttpResponseMessage resposta = requisicao.PostAsync(URL, param).GetAwaiter().GetResult();
 
             if (resposta.StatusCode == HttpStatusCode.OK)
             {
@@ -33,5 +35,35 @@ namespace XFApp_NossoChat.Service
             }
             return null;
         }
+
+        public static List<Chat> GetChats()
+        {
+            var URL = EnderecoBase + "/chats";
+
+            HttpClient requisicao = new HttpClient();
+            HttpResponseMessage resposta = requisicao.GetAsync(URL).GetAwaiter().GetResult();
+
+            if (resposta.StatusCode == HttpStatusCode.OK)
+            {
+                string conteudo = resposta.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                if (conteudo.Length > 2)
+                {
+                    List<Chat> lista = JsonConvert.DeserializeObject<List<Chat>>(conteudo);
+                    return lista;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
+           
+               
+
+            
